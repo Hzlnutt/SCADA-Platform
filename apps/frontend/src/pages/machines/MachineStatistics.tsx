e3283dad0597075dc006d893f8aabb369d09b32c
Salman Akbar <salmanspenido@gmail.com>
2026-08-12 11:18:15 +0700

    fix: add useCallback to React import in MachineStatistics.tsx

--- apps/frontend/src/pages/machines/MachineStatistics.tsx
+++ apps/frontend/src/pages/machines/MachineStatistics.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { Bar, Line } from "react-chartjs-2";
 import { getUnitById } from "../../data/machines";
